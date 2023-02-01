d6ae1f69f261816c8e7d36c32b20efa1253c6c08
“immanuel-savvy” <immanuelsavvy@gmail.com>
2023-02-01 14:30:01 +0100

    1675258200963244310

--- src/sections/student_loan_calculator.js
+++ src/sections/student_loan_calculator.js
@@ -10,9 +10,9 @@ class Student_loan_calculator extends React.Component {
     for (let i = 1; i < 15; i++) years.push(this_year - i);
 
     this.state = {
-      loan_amount: "50000",
-      annual_income: "24000",
-      annual_increment: "2.5",
+      loan_amount: "30000",
+      annual_income: "80000",
+      annual_increment: "0",
       loan_interest: "1.25",
       years,
       loan_type: "1",
@@ -29,8 +29,8 @@ class Student_loan_calculator extends React.Component {
     "salary",
     "debt",
     "int._rate (%)",
-    "monthly_payment",
-    "paid_this_year",
+    "monthly_repayment",
+    "annual_repayment",
     "interest_this_year",
     "total_paid",
     "total_interest"
@@ -77,23 +77,24 @@ class Student_loan_calculator extends React.Component {
       year: this_year,
     };
 
-    if (annual_income < threshold) initial_repayment.paid_this_year = 0;
+    if (annual_income < threshold) initial_repayment.annual_repayment = 0;
     else
-      initial_repayment.paid_this_year = Math.floor(
+      initial_repayment.annual_repayment = Math.floor(
         (annual_income - threshold) * repayment_percentage
       );
 
-    initial_repayment.total_paid = initial_repayment.paid_this_year;
+    initial_repayment.total_paid = initial_repayment.annual_repayment;
     initial_repayment.interest_this_year = Math.floor(
       loan_amount * loan_interest
     );
     initial_repayment.total_interest = initial_repayment.interest_this_year;
 
-    initial_repayment.monthly_payment = (
-      initial_repayment.paid_this_year / 12
+    initial_repayment.monthly_repayment = (
+      initial_repayment.annual_repayment / 12
     ).toFixed(2);
 
-    let result = new Array(initial_repayment);
+    let result = new Array(initial_repayment),
+      should_break;
 
     for (let y = year; y < year + duration - 1; y++) {
       if (y < this_year) continue;
@@ -103,27 +104,37 @@ class Student_loan_calculator extends React.Component {
 
       repayment.debt =
         prior_repayment.debt -
-        prior_repayment.paid_this_year +
+        prior_repayment.annual_repayment +
         prior_repayment.interest_this_year;
       repayment.year = prior_repayment.year + 1;
       repayment.salary = Math.floor(
         prior_repayment.salary * annual_increment + prior_repayment.salary
       );
 
-      if (repayment.salary < threshold) repayment.paid_this_year = 0;
+      if (repayment.salary < threshold) repayment.annual_repayment = 0;
       else
-        repayment.paid_this_year = Math.floor(
+        repayment.annual_repayment = Math.floor(
           (repayment.salary - threshold) * repayment_percentage
         );
 
       repayment.total_paid =
-        prior_repayment.total_paid + repayment.paid_this_year;
+        prior_repayment.total_paid + repayment.annual_repayment;
       repayment.interest_this_year = Math.floor(repayment.debt * loan_interest);
       repayment.total_interest =
         prior_repayment.total_interest + repayment.interest_this_year;
 
-      repayment.monthly_payment = (repayment.paid_this_year / 12).toFixed(2);
+      if (repayment.annual_repayment >= repayment.debt) {
+        repayment.annual_repayment = repayment.debt;
+        should_break = true;
+      }
+
+      repayment.monthly_repayment = (repayment.annual_repayment / 12).toFixed(
+        2
+      );
+
       result.push(repayment);
+
+      if (should_break) break;
     }
 
     this.setState(
@@ -248,27 +259,14 @@ class Student_loan_calculator extends React.Component {
                     }}
                     aria-valuenow="20"
                   >
-                    <option value="1">Type 1</option>
-                    <option value="2">Type 2</option>
-                    <option value="3">Scotish Loan</option>
+                    <option value="1">
+                      Plan 1 (England & Wales Pre 2012+)
+                    </option>
+                    <option value="2">Plan 2 (England & Wales 2012+)</option>
+                    <option value="4">Postgraduate Loan</option>
+                    <option value="3">Plan 4 (Scottish students)</option>
                   </select>
                 </div>
-              </span>
-              <span style={{ marginLeft: 5, width: "100%" }}>
-                <label for="port number">Year of graduation:</label>
-
-                <select
-                  onChange={({ target }) => {
-                    this.setState({ year: Number(target.value) });
-                  }}
-                  aria-valuenow="20"
-                >
-                  {years.map((year) => (
-                    <option key={year} value={`${year}`}>
-                      {year}
-                    </option>
-                  ))}
-                </select>
               </span>
             </div>
 
@@ -280,6 +278,35 @@ class Student_loan_calculator extends React.Component {
                 Clear <i className="material-icons-outlined">close</i>
               </a>
             </span>
+
+            <p className="mt-4" style={{ textAlign: "justify" }}>
+              <span
+                style={{
+                  fontStyle: "italic",
+                  fontWeight: "bold",
+                }}
+              >
+                Disclaimer:
+              </span>{" "}
+              Please be advised that this tool should not be considered
+              financial advice. The results generated by this tool are not a
+              substitute for conducting your research and consulting official
+              sources, such as the UK Government website. Keep in mind that this
+              tool makes certain assumptions and the results may not be
+              completely accurate. The assumptions made by the tool may become
+              outdated if government policies change. This tool is not
+              guaranteed to be perfect and its use is at your own risk. We
+              cannot be held responsible for any negative consequences that may
+              occur. To learn more about UK student loans, please visit
+              <a
+                href="https://www.gov.uk/repaying-your-student-loan"
+                target="_blank"
+              >
+                {" "}
+                https://www.gov.uk/repaying-your-student-loan
+              </a>
+              .
+            </p>
           </form>
           <div className="text sm_screen">
             <p className="title">About Student Loan Calculator</p>
