7f03192d2885871a902816c0853aee32692eaff0
“immanuel-savvy” <immanuelsavvy@gmail.com>
2023-04-11 12:15:51 +0100

    1681211751579191038

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Techmaster from "./Techmaster";
 import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga";
+
+const measurement_id = "G-XKSGORRQYL";
+ReactGA.initialize(measurement_id);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Techmaster />);
 
+const send_analytics = () =>
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(send_analytics);
